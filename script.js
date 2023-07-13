e26730234ead57a5400958f05a1a4b00a523cc39
Isaiah Henson <Devpuddles@gmail.com>
2023-07-13 13:31:04 -0400

    refactored the code to include 5 matches inside one function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let computerChoice;
+let playerChoice;
+
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 30) + 1
@@ -11,24 +13,6 @@ function getComputerChoice() {
         computerChoice = 'scissors'
     }
 }
-getComputerChoice()
-console.log(`The computer's choice is: ${computerChoice}`)
-
-let playerChoice;
-
-function getPlayerChoice() {
-    let randomNum = Math.floor(Math.random() * 30) + 1
-
-    if (randomNum <= 10) {
-        playerChoice = 'rock'
-    } else if (randomNum <= 20) {
-        playerChoice = 'paper'
-    } else {
-        playerChoice = 'scissors'
-    }
-}
-getPlayerChoice()
-console.log(`The player's choice is: ${playerChoice}`)
 
 function letsGo(computer, player) {
     if (computer === player) {
@@ -41,9 +25,42 @@ function letsGo(computer, player) {
         return 'The computer is the winner!'
     } else if (computer == 'paper' && player == 'rock') {
         return 'The computer is the winner!'
-    } else if ( computer == 'paper' && player == 'scissors'){
+    } else if (computer == 'paper' && player == 'scissors') {
         return 'The player is the winner'
     }
 }
 
-console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+function game() {
+    getComputerChoice()
+    console.log(`The computer's choice is: ${computerChoice}`)
+    playerChoice = prompt('rock, paper, or scissors').toLowerCase();
+    console.log(`The player's choice is: ${playerChoice}`)
+    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+    getComputerChoice()
+    playerChoice = prompt('rock, paper, or scissors').toLowerCase();
+    console.log(`The player's choice is: ${playerChoice}`)
+    console.log(`The computer's choice is: ${computerChoice}`)
+    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+    getComputerChoice()
+    console.log(`The computer's choice is: ${computerChoice}`)
+    playerChoice = prompt('rock, paper, or scissors').toLowerCase();
+    console.log(`The player's choice is: ${playerChoice}`)
+    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+    getComputerChoice()
+    console.log(`The computer's choice is: ${computerChoice}`)
+    playerChoice = prompt('rock, paper, or scissors').toLowerCase();
+    console.log(`The player's choice is: ${playerChoice}`)
+    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+    getComputerChoice()
+    console.log(`The computer's choice is: ${computerChoice}`)
+    playerChoice = prompt('rock, paper, or scissors').toLowerCase();
+    console.log(`The player's choice is: ${playerChoice}`)
+    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+
+}
+game()
