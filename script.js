d906ecc90c18985c29dc2757cab172a9ad289d3d
Isaiah Henson <Devpuddles@gmail.com>
2023-07-13 12:52:15 -0400

    completed a rock paper scissors game in the console

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('Hello World')
+let computerChoice;
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 30) + 1
+
+    if (randomNum <= 10) {
+        computerChoice = 'rock'
+    } else if (randomNum <= 20) {
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors'
+    }
+}
+getComputerChoice()
+console.log(`The computer's choice is: ${computerChoice}`)
+
+let playerChoice;
+
+function getPlayerChoice() {
+    let randomNum = Math.floor(Math.random() * 30) + 1
+
+    if (randomNum <= 10) {
+        playerChoice = 'rock'
+    } else if (randomNum <= 20) {
+        playerChoice = 'paper'
+    } else {
+        playerChoice = 'scissors'
+    }
+}
+getPlayerChoice()
+console.log(`The player's choice is: ${playerChoice}`)
+
+function letsGo(computer, player) {
+    if (computer === player) {
+        return 'TIE GAME!'
+    } else if (computer == 'rock' && player == 'scissors' || player == 'paper') {
+        return 'The computer is the winner!'
+    } else if (computer == 'scissors' && player == 'rock') {
+        return 'The player is the winner!'
+    } else if (computer == 'scissors' && player == 'paper') {
+        return 'The computer is the winner!'
+    } else if (computer == 'paper' && player == 'rock') {
+        return 'The computer is the winner!'
+    } else if ( computer == 'paper' && player == 'scissors'){
+        return 'The player is the winner'
+    }
+}
+
+console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
