5202152fb567e7987ac15f7b8a0604cd93a6e147
Isaiah Henson <Devpuddles@gmail.com>
2023-10-29 12:42:19 -0400

    altered the content append results and choices to the DOM

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let divContainer = document.createElement('button')
 let rockBtn = document.createElement('button')
 let paperBtn = document.createElement('button')
 let scissorsBtn = document.createElement('button')
+let resultDiv = document.createElement('div')
 
 //giving btn text content
 
@@ -17,21 +18,28 @@ paperBtn.textContent = 'PAPER'
 scissorsBtn.textContent = 'SCISSORS'
 
 //styling elements in the DOM
-// divContainer.style.height = "50%"
+divContainer.style.height = "50%"
 divContainer.style.width = "80%"
 divContainer.style.position = "absolute";
 divContainer.style.top = "50%";
 divContainer.style.left = "50%";
 divContainer.style.transform = "translate(-50%, -50%)";
 divContainer.style.border = "2px solid black";
+//div within the containerDiv
+resultDiv.style.height = "30%"
+resultDiv.style.width = "80%"
+resultDiv.style.position = "absolute";
+resultDiv.style.top = "28%";
+resultDiv.style.left = "50%";
+resultDiv.style.transform = "translate(-50%, -50%)";
+resultDiv.style.border = "1px solid gray";
 
 //appending element to the DOM
 body.appendChild(divContainer);
 divContainer.appendChild(rockBtn)
 divContainer.appendChild(paperBtn)
 divContainer.appendChild(scissorsBtn)
-
-
+divContainer.insertBefore(resultDiv, rockBtn )
 
 
 function getComputerChoice() {
@@ -64,12 +72,28 @@ function letsGo(computer, player) {
 
 
 function game(playerChoice) {
+
     getComputerChoice()
-    console.log(`The computer's choice is: ${computerChoice}`)
-    console.log(`The player's choice is: ${playerChoice}`)
-    console.log(`Who Wins: ${letsGo(computerChoice, playerChoice)}`)
+    let npc = `The computer's choice is: ${computerChoice}`
+    let player = `The player's choice is: ${playerChoice}`
+    let results = `Who Wins: ${letsGo(computerChoice, playerChoice)}`
+
+    let npcElement = document.createElement('p')
+    let playerElement = document.createElement('p')
+    let resultsElement = document.createElement('p')
+
+    npcElement.textContent = npc
+    playerElement.textContent = player
+    resultsElement.textContent = results
+
+    resultDiv.innerHTML = ""
+
+    resultDiv.appendChild(npcElement)
+    resultDiv.appendChild(playerElement)
+    resultDiv.appendChild(resultsElement)
 
 }
+
 
 //adding event handlers to buttons
 
